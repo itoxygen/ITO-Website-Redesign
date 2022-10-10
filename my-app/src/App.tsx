3ab5fda772300794885c88766ec6592eb4bca157
ColinKnudsen <77690953+ColinKnudsen@users.noreply.github.com>
2022-10-10 17:04:49 -0400

    start

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="div1">
+          Isaiah's Section
+        </div>
+        <div className="div2">
+          Nick's Section
+        </div>
+        <div className="div3">
+          Noah's Section
+        </div>
     </div>
   );
 }
