559637e1474419abb7c7d5c8d23ef40938365225
paghinizonda@gmail.com <Colin-Knudsen>
2022-11-01 10:34:48 -0400

    almost done left side

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
         <div className="div1">
-          Isaiah's Section
+           <div className = "Welcome">Welcome to<br></br>the IT Oxygen<br></br>Enterprise.</div>
+           <div className = "JoinUs">Join us in person in Dillman 214 every Wednesday at 5pm.</div>
+           <div className="ContactUs-Discord">
+            <button className="ContactUsButton">Contact Us</button>
+            <div className="Spacer"></div>
+            <div className="JoinDiscord">Join Our Discord</div>
+            <div className="RightArrowDiscord">➔</div>
+           </div>
         </div>
         <div className="div2">
           Nick's Section
